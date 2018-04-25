a8f0bbd536159ea6bc19e785147bc471941c4af0
unknown <708863861@qq.com>
2018-04-25 18:46:44 +0800

    Fix Dashboard

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 app_name = 'Dashboard'
 urlpatterns = [
-  #  path('', views.dashboard_login,name='login' ),
-    path('login/',views.dashboard_login.as_view()),
-    # path('logout/', views.dashboard_logout, name='logout'),
-    path('logout/', views.dashboard_logout.as_view()),
-    path('index/', views.dashboard_index, name='index'),
+#  path('', views.dashboard_login,name='login' ),
+    path('login/',views.dashboard_login_view.as_view()),
+# path('logout/', views.dashboard_logout, name='logout'),
+    path('logout/', views.dashboard_logout_view.as_view()),
+# path('index/', views.dashboard_index, name='index'),
+    path('index/', views.dashboard_index_view.as_view()),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -4,9 +4,9 @@ from django.contrib import messages
 from django.contrib.auth.decorators	import login_required
 from .sys import sys
 from django.contrib.auth.views import LoginView,LogoutView
-
+from django.views.generic.base import TemplateView
 # Create your views here.
-class dashboard_login(LoginView):
+class dashboard_login_view(LoginView):
     template_name = 'Dashboard/login.html'
     redirect_authenticated_user = True
 # def dashboard_login(request):
@@ -33,21 +33,33 @@ class dashboard_login(LoginView):
 #     else:
 #         return render(request, 'Dashboard/login.html')
 
-class dashboard_logout(LogoutView):
+class dashboard_logout_view(LogoutView):
      template_name = 'Dashboard/login.html'
      next_page = '/dashboard/login'
 # def dashboard_logout(request):
 #     logout(request)
 #     messages.success(request, 'logout success')
 #     return redirect('/dashboard/')
+# def dashboard_index(request):
+#     if request.user.is_authenticated :
+#         context = {
+#             "sysinfo": sys(),
+#             "user_last_login": request.user.last_login,
+#         }
+#         return render_to_response('Dashboard/index.html', context)
+#     else:
+#         return redirect('Dashboard:login')
 
-@login_required
-def dashboard_index(request):
-    if request.user.is_authenticated :
-        context = {
-            "sysinfo": sys(),
-            "user_last_login": request.user.last_login,
-        }
-        return render_to_response('Dashboard/index.html', context)
-    else:
-        return redirect('Dashboard:login')
+class dashboard_index_view(TemplateView):
+    template_name = 'Dashboard/index.html'
+    def get_context_data(self, **kwargs,):
+        context = super().get_context_data(**kwargs)
+        context['sysinfo'] = sys()
+        context['user_last_login'] = self.request.user.last_login
+        return context
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
+        else:
+            return redirect('/dashboard/login')
