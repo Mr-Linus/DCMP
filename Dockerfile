d1de6aaa2c397dbd0a143faf368ea7b4d611206c
Funky <708863861@qq.com>
2019-01-29 10:39:25 +0800

    update travis cofig && add build image func

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.4-jessie
+FROM python:3.6-jessie
 
 ENV TZ="Asia/Shanghai"
 
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-
+echo "Pushing Images"
+sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/dcmp:latest .
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/dcmp:latest
+sudo docker logout
