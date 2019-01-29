3d13404a420a909d29f62a99915e7ae03fd377a5
Funky <708863861@qq.com>
2019-01-29 10:18:14 +0800

    update travis cofig && add build image func

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker build -t . registry.cn-hangzhou.aliyuncs.com/geekcloud/dcmp:latest
+sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/dcmp:latest .
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/dcmp:latest
